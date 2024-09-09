684c9617c01e44e9f6d6330bb54f108da5cb2f88
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-09-09 20:24:28 +0900

    feat(lotto-controller): 로또 구입 금액 입력 값 검증

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,21 @@
 package lotto.controller;
 
+import lotto.service.PurchaseAmountValidator;
 import lotto.view.InputView;
 
 public class LottoController {
+    private final PurchaseAmountValidator purchaseAmountValidator;
+
+    public LottoController() {
+        this.purchaseAmountValidator = new PurchaseAmountValidator();
+    }
+
     public void run() {
+        inputPurchaseAmount();
+    }
+
+    private void inputPurchaseAmount() {
         String input = InputView.inputPurchaseAmount();
+        purchaseAmountValidator.validatePurchaseAmount(input);
     }
 }
