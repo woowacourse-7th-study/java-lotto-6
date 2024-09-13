6488998d610e0fe7fe696953f8b5989762f3037a
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-09-13 19:10:59 +0900

    test(purchase-price-test): PurchasePrice 클래스에 대한 도메인 로직 테스트 작성

---
+++ src/test/java/lotto/domain/model/PurchasePriceTest.java
@@ -0,0 +1,38 @@
+package lotto.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static lotto.constant.ErrorMessage.ENTER_THOUSAND;
+import static lotto.constant.Number.UNIT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchasePriceTest {
+    @DisplayName("1000 단위의 입력이 아닌 경우 예외를 발생시킨다.")
+    @Test
+    void createPurchasePriceByNonThousand() {
+        // given
+        int price = UNIT.getValue() + 500;
+
+        // when & then
+        assertThatThrownBy(() -> new PurchasePrice(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ENTER_THOUSAND.toString());
+    }
+
+    @DisplayName("구매 가격을 1000 단위로 나누어 구매량을 구할 수 있다.")
+    @Test
+    void getQuantityByPurchasePrice() {
+        // given
+        int price = 8000;
+        Integer expected = price / UNIT.getValue();
+
+        // when
+        PurchasePrice purchasePrice = new PurchasePrice(price);
+        Integer real = purchasePrice.getQuantity();
+
+        // then
+        assertThat(real).isEqualTo(expected);
+    }
+}
